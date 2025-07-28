a0b3fb2e5f139f72d2c0f6ec3f0132872bbe086f
Peter Kilian <peterjohnkilian.work@gmail.com>
2025-07-29 01:33:40 +0200

    fix(general): general fixes and improvements

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,7 +7,11 @@
 - Basic project template
 - This changelog
 - MIT Licence
+- Added config.rs and editor.rs
 
 ### Changed
 
 - README.md has been changed from the default, generated one.
+- app.html got changed because... it had the wrong title! 
+- main.rs was changed to use a Builder
+- +page.svelte has nothing in it now :D
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,10 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod editor;
+mod config;
+
 fn main() {
-    rosing_editor_app_lib::run()
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
